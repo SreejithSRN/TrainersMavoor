fad40136b709e7b923dbce547813acde047736b5
SreejithSRN <sreejimavoor@gmail.com>
2023-12-27 19:56:31 +0530

    db changed to atlas

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.mongourl).then(() => {
-    console.log("Database Connected Succesfully")
+mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Database Connected Successfully:", process.env.mongourl);
 }).catch((err) => {
     console.log(err);
-})
+});
